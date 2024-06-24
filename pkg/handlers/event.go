b88c7db25222ea264f78477336814f6231c1b533
gafonso <91420802+SelectGabriel@users.noreply.github.com>
2024-06-23 22:13:14 -0300

    event implementation & /reset made correcly

--- pkg/handlers/event.go
+++ pkg/handlers/event.go
@@ -1,13 +1,51 @@
 package handlers
 
 import (
+	"EBX-API/pkg/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Event(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "all good here!",
-	})
+	var event struct {
+		Type        string `json:"type"`
+		Destination string `json:"destination,omitempty"`
+		Origin      string `json:"origin,omitempty"`
+		Amount      int    `json:"amount"`
+	}
+
+	if err := c.BindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//checa operação e chega validade
+	switch event.Type {
+	case "deposit":
+		models.Accounts[event.Destination] += event.Amount
+		c.JSON(http.StatusCreated, gin.H{"destination": gin.H{"id": event.Destination, "balance": models.Accounts[event.Destination]}})
+	case "withdraw":
+		balance, exists := models.Accounts[event.Origin]
+		if !exists || balance < event.Amount {
+			c.JSON(http.StatusNotFound, 0)
+			return
+		}
+		models.Accounts[event.Origin] -= event.Amount
+		c.JSON(http.StatusCreated, gin.H{"origin": gin.H{"id": event.Origin, "balance": models.Accounts[event.Origin]}})
+	case "transfer":
+		originBalance, originExists := models.Accounts[event.Origin]
+		if !originExists || originBalance < event.Amount {
+			c.JSON(http.StatusNotFound, 0)
+			return
+		}
+		models.Accounts[event.Origin] -= event.Amount
+		models.Accounts[event.Destination] += event.Amount
+		c.JSON(http.StatusCreated, gin.H{
+			"origin":      gin.H{"id": event.Origin, "balance": models.Accounts[event.Origin]},
+			"destination": gin.H{"id": event.Destination, "balance": models.Accounts[event.Destination]},
+		})
+	default:
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event type"})
+	}
 }
--- pkg/handlers/reset.go
+++ pkg/handlers/reset.go
@@ -7,8 +7,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
 func Reset(c *gin.Context) {
 	//reseta estrutura
 	models.Accounts = make(map[string]int)
-	c.Status(http.StatusOK)
+	//não conehcia o c.String, demorei para acertar esse teste!1
+	c.String(http.StatusOK, "OK")
+	//
 }
